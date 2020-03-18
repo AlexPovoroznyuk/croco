4bc01e6ce1d881ad75ddad073d181f4c92de887a
Alex <lexesss64@gmail.com>
2020-03-18 19:31:09 +0200

    fixes

--- src/js/custom/main.js
+++ src/js/custom/main.js
@@ -1,164 +1,112 @@
-if ($("#timer")) {
-    var getCountdown = function getCountdown() {
-
-        var current_date = new Date().getTime();
-        var seconds_left = (target_date - current_date) / 1000;
-
-        days = pad(parseInt(seconds_left / 86400));
-        seconds_left = seconds_left % 86400;
-
-        hours = pad(parseInt(seconds_left / 3600));
-        seconds_left = seconds_left % 3600;
-
-        minutes = pad(parseInt(seconds_left / 60));
-        seconds = pad(parseInt(seconds_left % 60));
-        if (target_date > current_date) {
-            countdown.html(`<span>${days}<b>days</b></span><span>${hours}<b>hours</b></span><span>${minutes}<b>minutes</b></span>`);
-        } else {
-            countdown.html(`<span>0<b>days</b></span><span>0<b>hours</b></span><span>0<b>minutes</b></span>`);
-        }
-    };
-
-    var pad = function pad(n) {
-        return (n < 10 ? '0' : '') + n;
-    };
-
-    var target_date = 1589235720000;
-    var days, hours, minutes, seconds;
-
-    var countdown = $("#timer");
-    getCountdown();
-
-    setInterval(function () {
+$(document).ready(function(){
+    var lang = $("#timer").attr("data-lang")
+    if ($("#timer")) {
+        var getCountdown = function getCountdown() {
+    
+            var current_date = new Date().getTime();
+            var seconds_left = (target_date - current_date) / 1000;
+    
+            days = pad(parseInt(seconds_left / 86400));
+            seconds_left = seconds_left % 86400;
+    
+            hours = pad(parseInt(seconds_left / 3600));
+            seconds_left = seconds_left % 3600;
+    
+            minutes = pad(parseInt(seconds_left / 60));
+            seconds = pad(parseInt(seconds_left % 60));
+            console.log(lang)
+            if(lang == "ru"){
+                 if (target_date > current_date) {
+                countdown.html(`<span>${days}<b>дней</b></span><span>${hours}<b>часов</b></span><span>${minutes}<b>минут</b></span>`);
+            } else {
+                countdown.html(`<span>0<b>дней</b></span><span>0<b>часов</b></span><span>0<b>минут</b></span>`);
+            }
+            }
+           else if (lang == "en"){
+            if (target_date > current_date) {
+                countdown.html(`<span>${days}<b>days</b></span><span>${hours}<b>hours</b></span><span>${minutes}<b>minutes</b></span>`);
+            } else {
+                countdown.html(`<span>0<b>days</b></span><span>0<b>hours</b></span><span>0<b>minutes</b></span>`);
+            }
+           }
+           else if (lang == "cn"){
+            if (target_date > current_date) {
+                countdown.html(`<span>${days}<b>天</b></span><span>${hours}<b>小時</b></span><span>${minutes}<b>分鐘</b></span>`);
+            } else {
+                countdown.html(`<span>0<b>天</b></span><span>0<b>小時</b></span><span>0<b>分鐘</b></span>`);
+            }
+           }
+           else if (lang == "tr"){
+            if (target_date > current_date) {
+                countdown.html(`<span>${days}<b>günler</b></span><span>${hours}<b>saat</b></span><span>${minutes}<b>dakika</b></span>`);
+            } else {
+                countdown.html(`<span>0<b>günler</b></span><span>0<b>saat</b></span><span>0<b>dakika</b></span>`);
+            }
+           }
+           else if (lang == "id"){
+            if (target_date > current_date) {
+                countdown.html(`<span>${days}<b>hari</b></span><span>${hours}<b>jam</b></span><span>${minutes}<b>menit</b></span>`);
+            } else {
+                countdown.html(`<span>0<b>hari</b></span><span>0<b>jam</b></span><span>0<b>menit</b></span>`);
+            }
+           }
+           else if (lang == "sp"){
+            if (target_date > current_date) {
+                countdown.html(`<span>${days}<b>dias</b></span><span>${hours}<b>horas</b></span><span>${minutes}<b>minutos</b></span>`);
+            } else {
+                countdown.html(`<span>0<b>dias</b></span><span>0<b>horas</b></span><span>0<b>minutos</b></span>`);
+            }
+           }
+        };
+    
+        var pad = function pad(n) {
+            return (n < 10 ? '0' : '') + n;
+        };
+    
+        var target_date = 1589235720000;
+        var days, hours, minutes, seconds;
+    
+        var countdown = $("#timer");
         getCountdown();
-    }, 1000);
-}
-
-
-$(".modal .close-trigger").click(function(){
-	$(".thk-modal").removeClass("active");
-	$(".err-modal").removeClass("active");
-	$(".subscr-modal").removeClass("active");
-	$("body").removeClass("modal-open");
+    
+        setInterval(function () {
+            getCountdown();
+        }, 1000);
+    }
 })
 
 
-$('.subscribe-wr form').on('submit', function (e) {
-	e.preventDefault()
+function fadeIn(){
+    if($(window).innerWidth() > 992){
+        if($(".fade-block").length){
+		var currentPosition = $(window).scrollTop() + $(window).innerHeight();
+	$(".fade-block").each(function(){
+        if($(this).hasClass("footer")){
+            if($(this).offset().top < currentPosition - 50){
+                $(this).addClass("active")
+            }
+        }
+        else{
+            if($(this).offset().top < currentPosition - 300){
+			$(this).addClass("active")
+		}
+        }
 	
-	if (!/^[A-Za-z0-9][A-Za-z0-9-_\.]+@([a-z]|[a-z0-9]?[a-z0-9-]+[a-z0-9])\.[a-z0-9]{2,10}(?:\.[a-z]{2,10})?$/.test($(this).find("input").val())) {
-		$(this).find("input").closest(".input-item").addClass("validation-error");
+	})
 	}
-	else{
-		$(this).find("input").closest(".input-item").removeClass("validation-error");
-		var emailData = new FormData();
-		emailData.append("email", "" + $(this).find("input").val());
-		$.ajax({
-			url: $(this).attr('action'),
-			type: 'POST',
-			contentType: false,
-			processData: false,
-			data: emailData,
-
-			success: function success(data) {
-				$(".overlay").addClass("hide")
-				$(".thk-modal").addClass("active");
-					$("body").addClass("modal-open");
-			},
-			error: function error(xhr, err, data) {
-				$(".err-modal").addClass("active");
-					$("body").addClass("modal-open");
-					$(".overlay").addClass("hide")
-			}
-		});
-	}
-
+    }
 	
 	
-	
-});
-
-
-var phone = function(val){
-		return /^(\s*)?(\+)?([-()]?\d[-()]?){10,14}(\s*)?$/.test(val);
 }
 
 
-var sendForm;
-$(".subscribe-form").on("submit", function(e){
-	e.preventDefault();
-	var thisForm = $(this);
-	sendForm=true;
-	$(this).find(".reqiered-field").each(function(){
+$(document).scroll(function(){
+	fadeIn();
+})
+$(document).ready(function(){
+	fadeIn();
+})
 
-		if($(this).val() ==""){
-			$(this).closest(".input-item").addClass("validation-error");
-			$(this).closest(".input-item").find(".error span").html(EmptyError);
-			sendForm=false;
-		}
-		else if($(this).attr("name") == "email"){
-			if($(this).val() ==""){
-				$(this).closest(".input-item").addClass("validation-error");
-				$(this).closest(".input-item").find(".error span").html(EmptyError);
-				sendForm=false;
-			}
-			else if(!(/^[a-z0-9][a-z0-9-_\.]+@([a-z]|[a-z0-9]?[a-z0-9-]+[a-z0-9])\.[a-z0-9]{2,10}(?:\.[a-z]{2,10})?$/.test($(this).val()))){
-				$(this).closest(".input-item").addClass("validation-error");
-				$(this).closest(".input-item").find(".error span").html(EmailError);
-				sendForm=false;
-			}
-			else{
-				$(this).closest(".input-item").removeClass("validation-error");
-			}
-		}
-		else if($(this).attr("data-name") == "tg"){
-
-			if(!tgValidation($(this).val())){
-				$(this).closest(".input-item").addClass("validation-error");
-				$(this).closest(".input-item").find(".error span").html(TgError);
-				sendForm=false;
-			}
-			else{
-				$(this).closest(".input-item").removeClass("validation-error");
-			}
-		}
-		else if($(this).attr("data-name") == "link"){
-			
-			if(!urlValidation($(this).val())){
-				$(this).closest(".input-item").addClass("validation-error");
-				$(this).closest(".input-item").find(".error span").html(UrlError);
-				sendForm=false;
-			}
-			else{
-				$(this).closest(".input-item").removeClass("validation-error");
-			}
-		}
-		else{
-			$(this).closest(".input-item").removeClass("validation-error");
-		}
-	});
-	if(sendForm){
-		var that = $(this);
-		var formData = new FormData(that.get(0));
-		$.ajax({
-			url: $(this).attr('action'),
-			type:'POST',
-			contentType: false,
-			processData: false,
-			data: formData,
-			
-			success : function( data ) {
-					$(".subscr-modal").removeClass("active");
-					$(".thk-modal").addClass("active");
-					$("body").addClass("modal-open");	
-			},
-			error   : function( xhr, err , data ) {
-					$(".subscr-modal").removeClass("active");
-					$(".err-modal").addClass("active");
-					$("body").addClass("modal-open");
-
-				
-			}
-		});
-	}
-});
+$(".coockies-wr .button").click(function(){
+    $(".coockies-wr").addClass("hidden-block")
+})
